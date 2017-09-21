74cc10e400a08a2d02c889e08fa9b027a83a9df7
YannigSmagghe <smagghe.yannig@gmail.com>
2017-09-21 22:27:44 +0200

    init with all template

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,4 +1,4 @@
-    <title>Matrix Admin</title>
+    <title>Kyrin Admin</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" >
